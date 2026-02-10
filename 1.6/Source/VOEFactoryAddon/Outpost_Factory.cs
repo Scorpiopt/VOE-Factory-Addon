7d3d492f3a3dd3e93b798084d048326c29a9a9fb
Taranchuk <nfhfyxer@gmail.com>
2026-02-10 18:36:12 +0200

    better tech level check

--- 1.6/Source/VOEFactoryAddon/Outpost_Factory.cs
+++ 1.6/Source/VOEFactoryAddon/Outpost_Factory.cs
@@ -14,13 +14,14 @@ namespace VOEFactoryAddon
     {
         private OutpostExtension_Factory ExtFactory => Ext as OutpostExtension_Factory;
         private List<ThingDef> candidates;
+        private static Dictionary<ThingDef, TechLevel> cachedTechLevelValues = new Dictionary<ThingDef, TechLevel>();
         public override IEnumerable<ResultOption> GetExtraOptions()
         {
             if (candidates == null)
             {
                 candidates = DefDatabase<ThingDef>.AllDefs.Where(t =>
                                 !t.IsBlueprint && !t.IsFrame && !t.IsCorpse && t.PlayerAcquirable &&
-                                (ExtFactory.allowedTechLevels.NullOrEmpty() || ExtFactory.allowedTechLevels.Contains(t.techLevel)) &&
+                                (ExtFactory.allowedTechLevels.NullOrEmpty() || ExtFactory.allowedTechLevels.Contains(GetTechLevelFor(t))) &&
                                 MatchesFactoryType(t)
                             ).ToList();
             }
@@ -73,6 +74,56 @@ namespace VOEFactoryAddon
                 default:
                     return false;
             }
+        }
+
+        public static TechLevel GetTechLevelFor(ThingDef thingDef)
+        {
+            if (!cachedTechLevelValues.TryGetValue(thingDef, out TechLevel techLevel))
+            {
+                cachedTechLevelValues[thingDef] = techLevel = GetTechLevelForInt(thingDef);
+            }
+            return techLevel;
+        }
+
+        private static TechLevel GetTechLevelForInt(ThingDef thingDef)
+        {
+            List<TechLevel> techLevelSources = new List<TechLevel>();
+            if (thingDef.GetCompProperties<CompProperties_Techprint>() != null)
+            {
+                techLevelSources.Add(thingDef.GetCompProperties<CompProperties_Techprint>().project.techLevel);
+            }
+
+            if (thingDef.recipeMaker != null)
+            {
+                if (thingDef.recipeMaker.researchPrerequisite != null)
+                {
+                    var techLevel = thingDef.recipeMaker.researchPrerequisite.techLevel;
+                    if (techLevel != TechLevel.Undefined)
+                    {
+                        techLevelSources.Add(techLevel);
+                    }
+                }
+                if (thingDef.recipeMaker.researchPrerequisites?.Any() ?? false)
+                {
+                    var num = thingDef.recipeMaker.researchPrerequisites.MaxBy(x => (int)x.techLevel).techLevel;
+                    var techLevel = (TechLevel)num;
+                    if (techLevel != TechLevel.Undefined)
+                    {
+                        techLevelSources.Add(techLevel);
+                    }
+                }
+            }
+            if (thingDef.researchPrerequisites?.Any() ?? false)
+            {
+                var num = thingDef.researchPrerequisites.MaxBy(x => (int)x.techLevel).techLevel;
+                var techLevel = (TechLevel)num;
+                if (techLevel != TechLevel.Undefined)
+                {
+                    techLevelSources.Add(techLevel);
+                }
+            }
+            techLevelSources.Add(thingDef.techLevel);
+            return techLevelSources.MaxBy(x => (int)x);
         }
     }
 }
